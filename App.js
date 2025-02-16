19591cc99aa1f55f7256640fc18f8e645989222e
Manu Jha <140480862+manujha15@users.noreply.github.com>
2025-02-17 00:33:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Changebg from './project/Changebg';
+
+function App() {
+  return (
+    <div className="App">
+      <Changebg/>
+    </div>
+  );
+}
+
+export default App;
